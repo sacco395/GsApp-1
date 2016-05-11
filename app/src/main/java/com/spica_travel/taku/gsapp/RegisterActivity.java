3115e4cba6e2d46f594f2eab12f6e9b7a3641944
佐々木 卓 <moog@fc4.so-net.ne.jp>
2016-05-11 23:59:39 +0900

    RegisterActivityにUserActivityから登録の部分を切り取って貼る

--- app/src/main/java/com/spica_travel/taku/gsapp/RegisterActivity.java
+++ app/src/main/java/com/spica_travel/taku/gsapp/RegisterActivity.java
@@ -29,8 +29,100 @@ public class RegisterActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        //SharePreferences prefは自動ログインのため保存されているaccess tokenを読み出す。tokenがあればログインできる
+        //紫色は定数
+        SharedPreferences pref = getSharedPreferences(getString(R.string.save_data_name), Context.MODE_PRIVATE);
+        String token = pref.getString(getString(R.string.save_token), "");//保存されていない時は""
+        //tokenがないとき。
+        if(token == "") {
+            //画面を作る
+            CreateMyView(savedInstanceState);
+        }else {
+            //自動ログインをする。
+            try {
+                //KiiCloudのAccessTokenによるログイン処理。完了すると結果がcallback関数として実行される。
+                KiiUser.loginWithToken(callback, token);
+            } catch (Exception e) {
+                //ダイアログを表示
+                showAlert(R.string.operation_failed, e.getLocalizedMessage(), null);
+                //画面を作る
+                CreateMyView(savedInstanceState);
+            }
+        }
+
     }
+    //Viewを作る。いつもonCreateでやっていること
+    protected void CreateMyView(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_register);
+        //EditTextのビューを探します
+        mUsernameField = (EditText) findViewById(R.id.username_field);
+        mPasswordField = (EditText) findViewById(R.id.password_field);
+        //パスワードを隠す設定
+        mPasswordField.setTransformationMethod(new PasswordTransformationMethod());
+        //パスワードの入力文字を制限する。参考：http://techbooster.jpn.org/andriod/ui/3857/
+        mPasswordField.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        //登録ボタン
+        Button signupBtn = (Button) findViewById(R.id.signup_button);
+        //登録ボタンをクリックした時の処理を設定
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //登録処理
+                onSignupButtonClicked(v);
+            }
+        });
+    }
+    //ダイアログを表示する
+    void showAlert(int titleId, String message, AlertDialogFragment.AlertDialogListener listener ) {
+        DialogFragment newFragment = AlertDialogFragment.newInstance(titleId, message, listener);
+        newFragment.show(getFragmentManager(), "dialog");
+    }
+    //登録処理
+    public void onSignupButtonClicked(View v) {
+        //IMEを閉じる
+        InputMethodManager imm = (InputMethodManager) this.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+
+        //入力文字を得る
+        String username = mUsernameField.getText().toString();
+        String password = mPasswordField.getText().toString();
+        try {
+            //KiiCloudのユーザ登録処理
+            KiiUser user = KiiUser.createWithUsername(username);
+            user.register(callback, password);
+        } catch (Exception e) {
+            showAlert(R.string.operation_failed, e.getLocalizedMessage(), null);
+        }
+    }
+    //新規登録、ログインの時に呼び出されるコールバック関数
+    KiiUserCallBack callback = new KiiUserCallBack() {
+        //新規登録の時に自動的に呼び出される
+        @Override
+        public void onRegisterCompleted(int token, KiiUser user, Exception e) {
+            if (e == null) {
+                //自動ログイン(ログイン前)のためにSharedPreferenceに保存。アプリのストレージ。参考：http://qiita.com/Yuki_Yamada/items/f8ea90a7538234add288
+                //.apply()で保存
+                SharedPreferences pref = getSharedPreferences(getString(R.string.save_data_name), Context.MODE_PRIVATE);
+                pref.edit().putString(getString(R.string.save_token), user.getAccessToken()).apply();
+
+                // Intent のインスタンスを取得する。getApplicationContext()で自分のコンテキストを取得。遷移先のアクティビティーを.classで指定
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                // 遷移先の画面を呼び出す
+                startActivity(intent);
+                //戻れないようにActivityを終了します。
+                //finishがないと戻るボタンでログイン画面に戻れてしまう
+                finish();
+            } else {
+                //eがKiiCloud特有のクラスを継承している時
+                if (e instanceof CloudExecutionException)
+                    //KiiCloud特有のエラーメッセージを表示
+                    showAlert(R.string.operation_failed, Util.generateAlertMessage((CloudExecutionException) e), null);
+                else
+                    //一般的なエラーを表示
+                    showAlert(R.string.operation_failed, e.getLocalizedMessage(), null);
+            }
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
